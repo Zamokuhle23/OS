0fd241458697ed7104c91efcef3a25f0b7eb9b78
Zamokuhle23 <65773452+Zamokuhle23@users.noreply.github.com>
2021-12-20 00:01:52 +0400

    Add files via upload

---
+++ script4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+   char c; 
+   int check = 0;   
+   FILE *fp=fopen(argv[1],"r");
+   
+   if(fp==NULL)
+   {
+      printf("Failed to open the file"); 
+   }
+   else
+   {
+       int add = 0;
+       while(c=fgetc(fp))
+       {  
+          if(c==EOF) break;
+          while(c != '\n'){
+              c=fgetc(fp);
+              printf("%c",c);
+              if(c==EOF) break;
+          }
+
+          check = check + 1;
+          if(check%(atoi(argv[2])) == 0 && check != 0){
+          printf("\n");
+           }
+          
+          
+       }
+       fclose(fp);
+       printf("Press any key to Exit\n");
+       getchar();
+   }
+}
